b7aa61359e1a4b55f9573f41079e048220c2bf9b
pjarrell <philip.jarrell@disney.com>
2018-05-21 13:31:25 -0400

    Move Login to components

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ import Typography from '@material-ui/core/Typography';
 import {withStyles} from '@material-ui/core/styles';
 import withRoot from '../withRoot';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
-import repos from 'store';
+import Login from 'components/Login/Login';
 
 const styles = theme => ({
     root: {
@@ -18,49 +18,6 @@ const styles = theme => ({
         paddingTop: theme.spacing.unit * 20,
     },
 });
-
-class Login extends React.Component {
-
-    componentDidMount() {
-        const firebase = repos.firebase();
-        if (firebase.auth().isSignInWithEmailLink(window.location.href)) {
-            // Additional state parameters can also be passed via URL.
-            // This can be used to continue the user's intended action before triggering
-            // the sign-in operation.
-            // Get the email if available. This should be available if the user completes
-            // the flow on the same device where they started it.
-            let email = window.localStorage.getItem('emailForSignIn');
-            if (!email) {
-                // User opened the link on a different device. To prevent session fixation
-                // attacks, ask the user to provide the associated email again. For example:
-                email = window.prompt('Please provide your email for confirmation');
-            }
-            // The client SDK will parse the code from the link for you.
-            firebase.auth().signInWithEmailLink(email, window.location.href)
-                .then(function (result) {
-                    // Redirect to landing
-                    window.location.replace('/');
-
-                    // Clear email from storage.
-                    //window.localStorage.removeItem('emailForSignIn');
-                    // You can access the new user via result.user
-                    // Additional user info profile not available via:
-                    // result.additionalUserInfo.profile == null
-                    // You can check if the user is new or existing:
-                    // result.additionalUserInfo.isNewUser
-                })
-                .catch(function (error) {
-                    // Some error occurred, you can inspect the code: error.code
-                    // Common errors could be invalid email and invalid or expired OTPs.
-                    console.error('Login failed', error);
-                });
-        }
-    }
-
-    render() {
-        return <h1>Logging in...</h1>;
-    }
-}
 
 class Index extends React.Component {
     state = {
