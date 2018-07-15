133f92ccccd68ec49c017bcc1a79c9385aa3a1b2
pjarrell <philip.jarrell@disney.com>
2018-07-14 21:49:54 -0400

    Add flow and update types

---
+++ src/types/AdminTypes.js
@@ -0,0 +1,13 @@
+import PropTypes from "prop-types";
+import {NameType, AttendeeType, ImageType, VideoType, CatalogType, CategoryType, ProductType} from 'types/Types';
+
+export {
+    NameType,
+    AttendeeType,
+    ImageType,
+    VideoType,
+    CatalogType,
+    CategoryType,
+    ProductType,
+};
+
