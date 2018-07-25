584471db041be81af8ce9440eb75d2cacaaa91ef
pjarrell <philip.jarrell@disney.com>
2018-07-24 23:37:10 -0400

    Beginning of admin

--- src/pages/Admin/DashboardPage.js
+++ src/pages/Admin/DashboardPage.js
@@ -1,6 +1,6 @@
 // @flow
 import * as React from 'react';
-import {Box, Text} from 'gestalt';
+import {Box, Column, Image, Text} from 'gestalt';
 import {defineMessages, FormattedMessage, injectIntl} from "react-intl";
 import type {IntLType} from "types/Types";
 
@@ -19,13 +19,69 @@ class DashboardPage extends React.Component<Props> {
     render() {
         return <Box direction="row"
                     display="flex"
-                    justifyContent="center"
-                    alignItems="center"
+                    flex="grow"
+                    wrap
                     height="100%">
-            <Text bold size="xl">
-                <FormattedMessage id={translations.title.id}
-                                  defaultMessage={translations.title.defaultMessage}/>
-            </Text>
+            <Column span={3} mdSpan={2}>
+                <Box color="darkGray"
+                     flex="grow"
+                     display="flex"
+                     direction="column"
+                     height="100%">
+                    <Box paddingX={10} paddingY={4}>
+                        <Image src="/static/images/frontdesk_logo.png"
+                               alt="Logo"
+                               naturalWidth={144}
+                               naturalHeight={152}/>
+                    </Box>
+                    <ul>
+                        <li>
+                            <Text bold color="white">
+                                Dashboard
+                            </Text>
+                        </li>
+                        <li>
+                            <Text bold color="white">
+                                Parties
+                            </Text>
+                        </li>
+                        <li>
+                            <Text bold color="white">
+                                Contacts
+                            </Text>
+                        </li>
+                        <li>
+                            <Text bold color="white">
+                                Orders
+                            </Text>
+                        </li>
+                        <li>
+                            <Text bold color="white">
+                                Reports
+                            </Text>
+                        </li>
+                    </ul>
+                </Box>
+            </Column>
+            <Column span={9} mdSpan={10}>
+                <Box flex="grow"
+                     display="flex"
+                     direction="column"
+                     height="100%">
+                    <Box color="blue" height={140} padding={6}>
+                        <Text bold size="xl" color="white">
+                            <FormattedMessage id={translations.title.id}
+                                              defaultMessage={translations.title.defaultMessage}/>
+                        </Text>
+                    </Box>
+                    <Box flex="grow" padding={6}>
+                        <Text bold size="xl">
+                            <FormattedMessage id={translations.title.id}
+                                              defaultMessage={translations.title.defaultMessage}/>
+                        </Text>
+                    </Box>
+                </Box>
+            </Column>
         </Box>;
     }
 }
